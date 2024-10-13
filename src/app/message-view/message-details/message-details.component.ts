128967947235631150816fb6b087b5ae320929fc
David Barrineau <david.barrineau@phx-defense.com>
2024-10-13 15:32:47 -0400

    Added view-only message type for unknown for queues that are receivied of unknown messages.

--- src/app/message-view/message-details/message-details.component.ts
+++ src/app/message-view/message-details/message-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NchsViewMessage } from 'src/app/messages/view/nchs-view-message';
 import { MessageConverter } from 'src/app/messages/message-converter';
+import { MessageType } from 'src/app/messages/icd/message-type';
 
 /**
  * Displays the details of a message.
@@ -32,8 +33,13 @@ export class MessageDetailsComponent implements OnChanges {
    */
   public ngOnChanges(changes: SimpleChanges): void {
     if (changes['message'] && this.message) {
-      const deviceMessage = MessageConverter.convert(this.message.message);
-      this.messageText = JSON.stringify(deviceMessage, null, 2);
+      if (this.message.message.envelope.msg_type === MessageType.Unknown) {
+        this.messageText = JSON.stringify(this.message, null, 2);
+      }
+      else {
+        const deviceMessage = MessageConverter.convert(this.message.message);
+        this.messageText = JSON.stringify(deviceMessage, null, 2);
+      }
     }
     else if (changes['topic'] && !this.topic) {
       this.clear();
